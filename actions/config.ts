2e8c412fb60327a5cde42cda4ee73370f2aa9de6
therajsoni <rajsoni909192@gmail.com>
2025-09-03 18:19:27 +0530

    connect db

--- actions/config.ts
+++ actions/config.ts
@@ -3,8 +3,8 @@ import mongoose from "mongoose";
 const connectToDB = async () => {
   try {
     const url = await mongoose.connect(
-      // "mongodb://appadm:ad%400808@110.227.194.109:2738"
-      "mongodb://localhost:27017/dbfallback"
+      "mongodb://appadm:ad%400808@110.227.194.109:2738"
+      // "mongodb://localhost:27017/dbfallback"
     );
     console.log("Connected to MongoDB:", url.connection.host);
   } catch (error) {
