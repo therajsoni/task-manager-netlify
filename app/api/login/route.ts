21280a1bed4957249a9f57aa3482eab860116326
therajsoni <rajsoni909192@gmail.com>
2025-09-04 10:24:29 +0530

    login issue

--- app/api/login/route.ts
+++ app/api/login/route.ts
@@ -5,7 +5,7 @@ export async function POST(request: Request) {
     const body = await request?.json();
     let { username } = body;
     const { password } = body
-    if (!username || password) {
+    if (!username || !password) {
       return NextResponse.json({
         message: "Username and password are required",
         status: 400,
